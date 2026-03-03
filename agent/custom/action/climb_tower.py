e1b2e9aeb0733d60ff14bc296c7de23cdfedc4a5
samipale <seisei_126@163.com>
2026-03-03 16:16:18 +0800

    咕咕咕咕

--- agent/custom/action/climb_tower.py
+++ agent/custom/action/climb_tower.py
@@ -3,6 +3,7 @@ from maa.custom_action import CustomAction
 from maa.context import Context
 import json
 import time
+import re
 
 
 @AgentServer.custom_action("shop_action_old")
@@ -1305,6 +1306,17 @@ class ShopAction(CustomAction):
         }
     }
 
+    DISCOUNT_TEXT = {
+        "cn": ["优惠"],
+        "tw": ["優惠"],
+        "en": ["SALE"],
+        "jp": ["割引"]
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.lang_type = None
+
     def run(
             self,
             context: Context,
@@ -1313,43 +1325,56 @@ class ShopAction(CustomAction):
         # 获取资源类型
         param_str = argv.custom_action_param
         param = json.loads(param_str)
-        lang_type = param.get("lang_type")
+        self.lang_type = param.get("lang_type")
 
         # 先检查是中途的商店还是最终商店
         shop_type = self._check_shop_type(context)
+
+        # 定义初始参数
+        reserve_coin = 0
+
         # 然后进入商店购物的页面
         context.run_task("星塔_节点_商店_点击商店购物_agent")
         # 开始第一轮购买
         while True:
             # 读取8个格子的信息
-            grids_infos = self._get_grids_infos(context)
+            grids_infos = self._get_grids_info(context)
 
-            # 开始8个格子的循环
-            for grid in self.GRID_ROIS:
-                image = context.tasker.controller.post_screencap().wait().get()
-                # 读取当前金币
-                current_coin = self._get_current_coin(context, image)
+            # 按照策略决定购买内容
+            # 方案1：购买所有潜能特饮
+            buy_list = [grid for grid in grids_infos if grid["item_name"] == "potential_drink"]
 
-                # 检查格子内容与价格
-                item_info = self._get_grids_infos(context, lang_type)
-                # 判断是否购买
+            # 读取当前金币
+            coin = self._get_current_coin(context)
+            if coin:
+                coin = max(0, coin - reserve_coin)
+            else:
+                print("无法读取金币，为保证爬塔质量，将中止任务")
+                context.tasker.post_stop()
+                return False
 
-                # 执行购买操作
+            # 执行购买操作
+            for item in buy_list:
+                if coin >= item["price"]:
+                    # 获取道具所属格子范围
+                    buy_result = self._buy_item(context, item["price_roi"])
+                    if buy_result:
+                        coin = coin - reserve_coin
+                    else:
+                        if not context.tasker.stopping:
+                            print("购买操作出现问题，为保证爬塔质量，将中止任务")
+                            context.tasker.post_stop()
+                        return False
 
             # 循环完毕后，根据是中途商店还是最终商店，决定是否刷新物品继续新一轮的购买
-            if shop_type == "regular":
+            # 金币不足也退出循环
+            if shop_type == "regular" or coin < 145:
                 break
             else:
                 context.run_task("星塔_节点_商店_点击刷新_agent")
 
-            # 检查是否中断任务
-            if context.tasker.stopping:
-                return False
-
         # 退回商店层主界面
-
-
-
+        context.run_task("星塔_节点_商店_返回商店层_agent")
         return True
 
     @staticmethod
@@ -1396,7 +1421,7 @@ class ShopAction(CustomAction):
                 image(nd.array): 截图
 
             Returns:
-                int: 当前金币数量
+                int | None: 当前金币数量，识别失败时返回None
         """
         if not image:
             image = context.tasker.controller.post_screencap().wait().get()
@@ -1412,94 +1437,284 @@ class ShopAction(CustomAction):
 
             # 检查是否中断任务
             if context.tasker.stopping:
-                return 0
+                return None
 
-        return 0
+        return None
 
-    def _get_grids_infos(self, context, lang_type, image = None,grid_rois = None):
+    def _get_grids_info(self, context):
         """
-            获取每个格子的道具信息
+            获取8个格子的道具信息
 
             Args:
                 context(Context): 上下文对象
-                image(nd.array): 截图，默认为None
-                grid_rois(list): 格子识别框范围，默认为None
 
             Returns:
                 list: 每个格子的道具信息，每个元素为一个字典，包含item_type, item_price, item_name
         """
-        # 获取参数
-        if not image:
-            image = context.tasker.controller.post_screencap().wait().get()
-        if not grid_rois:
-            grid_rois = self.GRID_ROIS
-        # 生成反向查找表
-        mapping = self.get_reverse_mapping(lang_type)
         # 初始化结果列表
-        grids_infos = []
+        grids_info = []
 
         # 开始8个格子的循环
-        for grid in grid_rois:
+        for i in range(len(self.GRID_ROIS)):
             # 读取识别框范围
-            # item_roi = grid["item_roi"]
+            grid = self.GRID_ROIS[i]
+            item_roi = grid["item_roi"]
             price_roi = grid["price_roi"]
             name_roi = grid["name_roi"]
 
-            # 初始化参数
-            item_quantity = 0
-            item_price = 0
-            item_name = ""
+            # 获取单个格子的道具信息并添加到结果列表
+            # 暂时不需要用discount_flag，所以用_接收
+            item_name, item_quantity, item_price, _ = self._get_single_grid_info(context, item_roi, price_roi, name_roi)
+            if item_name and item_quantity and item_price:
+                discount = self._get_discount(item_name, item_quantity, item_price)
+                grids_info.append({
+                    "grid_num": i,
+                    "item_name": item_name,
+                    "item_quantity": item_quantity,
+                    "item_price": item_price,
+                    "discount": discount,
+                    "item_roi": item_roi,
+                    "price_roi": price_roi,
+                    "name_roi": name_roi,
+                })
+            else:
+                print(f"第{i}个格子内容识别失败")
+                print(f"item_name: {item_name}, item_quantity: {item_quantity}, item_price: {item_price}")
 
-            # 识别名字
-            reco_detail = context.run_recognition("星塔_节点_商店_识别物品名称_agent", image, {
-                "星塔_节点_商店_识别物品名称_agent": {
-                    "recognition": {
-                        "param": {
-                            "roi": name_roi
-                        }
+            # 根据价格从低到高排序
+            grids_info.sort(key=lambda x: int(x["item_price"]))
+
+        return grids_info
+
+    def _get_single_grid_info(self, context, item_roi, price_roi, name_roi, retry_max=3):
+        """
+            格子内容识别函数
+
+            Args:
+                context(Context): 上下文对象
+                item_roi(list): 整个道具格子的识别框范围
+                price_roi(list): 道具价格的识别框范围
+                name_roi(list): 道具名的识别框范围
+                retry_max(int): 最大重试次数，默认为3
+
+            Returns:
+                tuple: 格子的道具信息，包含item_name, item_quantity, item_price, discount_flag
+        """
+
+        # 方法一，通过整个道具格子识别
+        item_price = []
+        item_name = ""
+        discount_flag = 0
+
+        image = context.tasker.controller.post_screencap().wait().get()
+        results = self._grid_recognition(context, image, item_roi)
+        for r in results:
+            if price_roi[0] <= r.box[0] <= price_roi[2] and price_roi[1] <= r.box[1] <= price_roi[3]:
+                item_price.append(r.text)
+            if name_roi[0] <= r.box[0] <= name_roi[2] and name_roi[1] <= r.box[1] <= name_roi[3]:
+                item_name += r.text
+            if r.text in self.DISCOUNT_TEXT[self.lang_type]:
+                discount_flag = 1
+
+        # 检查并清洗识别结果
+        item_name, item_quantity = self._parse_item_name(item_name)
+        item_price = self._parse_item_price(item_price)
+
+        # 如果都识别到了，直接返回
+        if item_name and item_quantity and item_price:
+            return item_name, item_quantity, item_price, discount_flag
+
+        # 未能检测到的内容，使用方法二识别
+        count = 0
+        while count < retry_max and (not item_price or not item_name or not item_quantity):
+            image = context.tasker.controller.post_screencap().wait().get()
+            # 识别价格
+            if not item_price:
+                results = self._grid_recognition(context, image, price_roi)
+                item_price = [r.text for r in results]
+                item_price = self._parse_item_price(item_price)
+            # 识别道具名字及数量
+            if not item_name or not item_quantity:
+                results = self._grid_recognition(context, image, name_roi)
+                item_name_temp = "".join([r.text for r in results])
+                item_name_temp, item_quantity_temp = self._parse_item_name(item_name_temp)
+                if not item_name:
+                    item_name = item_name_temp
+                if not item_quantity:
+                    item_quantity = item_quantity_temp
+
+            # 如果都识别到了，直接返回
+            if item_name and item_quantity and item_price:
+                return item_name, item_quantity, item_price, discount_flag
+
+            # 检查是否中断任务
+            if context.tasker.stopping:
+                return None, None, None, None
+
+            # 睡觉
+            time.sleep(1)
+            count += 1
+
+        # 两个方法都没办法识别完整，返回原值
+        return item_name, item_quantity, item_price, discount_flag
+
+    @staticmethod
+    def _grid_recognition(context, image, roi, expected=r".+"):
+        """
+            格子内容识别函数
+
+            Args:
+                context(Context): 上下文对象
+                image(Image): 截图
+                roi(list): 识别框范围
+                expected(str): 预期的正则表达式，默认为".+"
+
+            Returns:
+                list: 可遍历的格子内容识别结果
+        """
+        reco_detail = context.run_recognition("星塔_节点_商店_识别物品内容_agent", image, {
+            "星塔_节点_商店_识别物品内容_agent": {
+                "recognition": {
+                    "param": {
+                        "expected": expected,
+                        "roi": roi
                     }
                 }
-            })
-            if reco_detail and reco_detail.hit:
-                for result in reco_detail.filtered_results:
-                    item_name += result.text
-            # 转换为内部通用名
+            }
+        })
+        if reco_detail and reco_detail.hit:
+            return reco_detail.filtered_results
+        return []
+
+    def _parse_item_name(self, item_name: str):
+        """
+            解析物品名称，提取物品名称和数量
+
+            Args:
+                item_name(str): 识别到的物品名称
+
+            Returns:
+                tuple: 解析后的物品名称和数量
+        """
+        # 生成反向查找表
+        mapping = self._get_reverse_mapping(self.lang_type)
+
+        # 获取物品数量
+        match = re.match(r"(.*?)\s*[x×]\s*(\d+)$", item_name, re.IGNORECASE)
+        item_quantity = 0
+
+        # 分离物品名称和数量
+        if match:
+            item_name = match.group(1).strip()
+            if match.group(2):
+                item_quantity = int(match.group(2).strip())
+
+        # 检查物品名称，尝试转换为程序内部通用名称，以适配不同服务端语言
+        if not item_name in self.ITEM_NAMES:
             item_name = mapping.get(item_name, "")
 
-            # 如果是音符，再去识别数量
-            if "melody" in item_name:
-                reco_detail = context.run_recognition("星塔_节点_商店_识别物品数量_agent", image, {
-                    "星塔_节点_商店_识别物品数量_agent": {
-                        "recognition": {
-                            "param": {
-                                "roi": name_roi
-                            }
-                        }
-                    }
-                })
-                if reco_detail and reco_detail.hit:
-                    item_quantity = reco_detail.best_result.text
+        # 针对潜能特饮，把数量改为1
+        if item_name == "potential_drink":
+            item_quantity = 1
 
-            # 识别价格
-            reco_detail = context.run_recognition("星塔_节点_商店_识别物品价格_agent", image, {
-                "星塔_节点_商店_识别物品价格_agent": {
-                    "recognition": {
-                        "param": {
-                            "roi": price_roi
-                        }
+        return item_name, item_quantity
+
+    @staticmethod
+    def _parse_item_price(item_price):
+        """
+            解析并提取物品价格
+
+            Args:
+                item_price(int | list): 识别到的物品价格
+
+            Returns:
+                int: 解析后的物品价格
+        """
+        if isinstance(item_price, int):
+            item_price = [str(item_price)]
+
+        parsed_item_price = []
+
+        for p in item_price:
+            # 排除为0的情况，往往是把硬币图标识别为0了
+            if p.isdecimal() and int(p) == 0:
+                continue
+
+            # 清洗掉可能识别到的打折价格，往往是跟原价混在一起了
+            # 一般价格为2~3位数，最便宜的音符*5是90块，最贵的音符*15是400块
+            # 例子："09045" -> 45, "9072" -> 72, "400200" -> 200, "0400320" -> 320
+            if len(p) >= 4:
+                while True:
+                    match = re.search(r'^.*?0(?=[1-9])', p)
+                    if match:
+                        p = p[match.end():]
+                    else:
+                        break
+                if p.isdecimal():
+                    parsed_item_price.append(int(p))
+
+            # 其他情况，直接转换为int
+            if p.isdecimal():
+                parsed_item_price.append(int(p))
+
+        # 对解析后的价格列表取最低值
+        return min(parsed_item_price, default=0)
+
+    @staticmethod
+    def _buy_item(context, roi):
+        """
+            使用pipeline执行购买操作
+
+            Args:
+                context(Context): 上下文对象
+                roi(list): 道具的点击范围
+
+            Returns:
+                bool: 是否完美执行购买操作
+        """
+        run_result = context.run_task("星塔_节点_商店_购买道具_agent", {
+            "星塔_节点_商店_购买道具_agent": {
+                "action": {
+                    "param": {
+                        "target": roi
                     }
                 }
-            })
-            if reco_detail and reco_detail.hit:
-                item_price = int(reco_detail.best_result.text)
+            }
+        })
+        if run_result and run_result.status.succeeded:
+            return True
+        return False
 
-            grids_infos.append({"item_name": item_name, "item_quantity": item_quantity, "item_price": item_price})
-
-        return grids_infos
-
-    def get_reverse_mapping(self, lang_type):
+    @staticmethod
+    def _get_discount(item_name, item_quantity, item_price):
         """
-        根据语言类型生成反向查找表
+            获取物品的折扣信息
+
+            Args:
+                item_name(str): 物品名称
+                item_quantity(int): 物品数量
+                item_price(int): 物品价格
+
+            Returns:
+                int: 折扣信息，0表示无折扣，1表示有折扣
+        """
+        # 检查是否为潜能特饮
+        if item_name == "potential_drink":
+            return item_price / 200.0
+
+        # 检查是否为音符
+        if "melody" in item_name and item_quantity == 5:
+            return  item_price / 90.0
+
+        if "melody" in item_name and item_quantity == 15:
+            return  item_price / 400.0
+
+        # 其他情况
+        return 1.0
+
+    def _get_reverse_mapping(self, lang_type):
+        """
+            根据语言类型生成反向查找表
         """
         reverse_map = {}
         items_dict = self.ITEM_NAMES
