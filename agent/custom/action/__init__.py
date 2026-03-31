455c4ac8b65a7e85180ac63c016cb45b55e4cd3b
samipale <seisei_126@163.com>
2026-03-31 18:08:04 +0800

    refactor(shop): 重构领取每日奖励流程，减少ocr依赖

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -1,12 +1,10 @@
 from .climb_tower import *
-from .shop import *
 from .invite import *
 from .fight import *
 
 __all__ = [
     "ShopAction",
     "EnhanceAction",
-    "DailyGiftAction",
     "InviteAuto",
     "UToolCalcRepeat",
     "AscensionPreparation",
